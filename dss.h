bcc04a9bca1623da6c7c33805662c66e7059a3b6
anadioti <angelos.anadiotis@epfl.ch>
2017-06-11 18:32:51 +0200

    Changed max lineitems per supplier to 1000 from 7 in case of skew

--- dss.h
+++ dss.h
@@ -373,7 +373,11 @@ extern tdef tdefs[];
 #define  O_CLRK_FMT   "%%s%%0%d%s"
 #define  O_CLRK_SCL      1000
 #define  O_LCNT_MIN      1
+#if ENABLE_SKEW
+#define  O_LCNT_MAX      1000
+#else
 #define  O_LCNT_MAX      7
+#endif
 
 /*
  * defines which control the lineitem table
